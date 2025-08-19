b9bcd741220f32f34c9d63e0b59612c8acf3169d
ymeister <47071325+ymeister@users.noreply.github.com>
2025-08-19 13:37:43 +0400

    Rework 'toHKD' and implement 'SomeHKD' pattern synonym

--- higher-kinded-data/src/HigherKinded/HKD/Construction.hs
+++ higher-kinded-data/src/HigherKinded/HKD/Construction.hs
@@ -5,10 +5,12 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ViewPatterns #-}
 
 -- | Based on 'Data.Generic.HKD.Construction from package 'higgledy'
 --   by Tom Harding ((c) Tom Harding, 2019, MIT)
@@ -50,13 +52,21 @@ import HigherKinded.HKT
 -- prop> construct (deconstruct x) == [ x :: (Int, Bool, String) ]
 class Construct (hkd :: (Type -> Type) -> Type) (structure :: Type) (f :: Type -> Type) where
   fromHKD :: hkd f -> f structure
-  toHKD :: structure -> hkd f
+  toHKD :: f structure -> hkd f
+
+pattern SomeHKD
+  :: forall hkd structure f.
+     Construct hkd structure f
+  => f structure
+  -> hkd f
+pattern SomeHKD { unSomeHKD } <- (fromHKD @hkd @structure @f -> unSomeHKD) where
+  SomeHKD x = toHKD @hkd @structure @f x
 
 
 
 instance ConstructHKD' structure hkt f => Construct (HKD structure hkt) structure f where
   fromHKD = fmap to . gFromHKD @(Rep structure) @hkt @f . unGHKD
-  toHKD = GHKD . gToHKD @(Rep structure) @hkt @f . from
+  toHKD = GHKD . gToHKD @(Rep structure) @hkt @f . fmap from
 
 class
     ( Applicative f
@@ -76,17 +86,17 @@ instance
 
 class GConstruct (rep :: Type -> Type) (hkt :: ((Type -> Type) -> Type -> Type)) (f :: Type -> Type) where
   gFromHKD :: GHKD_ rep hkt f () -> f (rep ())
-  gToHKD :: rep () -> GHKD_ rep hkt f ()
+  gToHKD :: f (rep ()) -> GHKD_ rep hkt f ()
 
 instance (Functor f, GConstruct inner hkt f)
     => GConstruct (M1 index meta inner) hkt f where
   gFromHKD = fmap M1 . gFromHKD @inner @hkt @f . unM1
-  gToHKD = M1 . gToHKD @inner @hkt @f . unM1
+  gToHKD = M1 . gToHKD @inner @hkt @f . fmap unM1
 
 instance (Applicative f, GConstruct left hkt f, GConstruct right hkt f)
     => GConstruct (left :*: right) hkt f where
   gFromHKD (l :*: r) = (:*:) <$> gFromHKD @left @hkt @f l <*> gFromHKD @right @hkt @f r
-  gToHKD (l :*: r) = gToHKD @left @hkt @f l :*: gToHKD @right @hkt @f r
+  gToHKD lr = gToHKD @left @hkt @f ((\(l :*: _) -> l) <$> lr) :*: gToHKD @right @hkt @f ((\(_ :*: r) -> r) <$> lr)
 
 instance
     ( Applicative f
@@ -98,5 +108,5 @@ instance
   =>
     GConstruct (K1 index inner) hkt f
   where
-    gFromHKD (K1 x) = fmap K1 $ fromHKT' @hkt @f @inner $ view _UnHKT' x
-    gToHKD (K1 x) = K1 (view _HKT' $ toHKT' @hkt @f @inner $ pure @f x)
+    gFromHKD = fmap K1 . fromHKT' @hkt @f @inner . view _UnHKT' . unK1
+    gToHKD = K1 . (view _HKT' . toHKT' @hkt @f @inner) . fmap unK1
